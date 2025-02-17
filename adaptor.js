6b8de929630cc604427b6f4219208a73c1db6f5b
orothy579 <ftsky2050@naver.com>
2025-02-17 08:42:39 +0000

    jason.parser test

--- adaptor.js
+++ adaptor.js
@@ -3,25 +3,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const mqtt_1 = __importDefault(require("mqtt"));
 const SERIAL_PORT = '/dev/ttyUSB0';
 const BAUD_RATE = 9600;
 const MQTT_BROKER_IP = process.env.MQTT_BROKER_IP || 'localhost';
 const MQTT_URL = `mqtt://{MQTT_BROKER_IP}:1883`;
-const ACCESS_TOKEN = 'YC6o3ZWlSKhCBrI3TwRk'; // device access token
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN; // device access token
 const client = mqtt_1.default.connect(MQTT_URL, {
     username: ACCESS_TOKEN,
 });
-let temperature = 20;
+let temp = 20;
 client.on('connect', () => {
     console.log('connected to thingsboard mqtt');
     setInterval(() => {
-        temperature += 1; // 온도를 1씩 증가
-        const payload = JSON.stringify({ temperature });
+        temp += 1;
+        const payload = JSON.stringify({ temp });
         client.publish('v1/devices/me/telemetry', payload, () => {
             console.log(`📡 Published: ${payload}`);
         });
-    }, 1000); // 1초마다 실행
+    }, 1000);
 });
 client.on('error', (err) => {
     console.error('Mqtt error:', err);
--- adaptor.ts
+++ adaptor.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import { SerialPort, ReadlineParser } from 'serialport'
 import mqtt from 'mqtt'
 
@@ -6,24 +8,23 @@ const BAUD_RATE = 9600
 
 const MQTT_BROKER_IP = process.env.MQTT_BROKER_IP || 'localhost'
 const MQTT_URL = `mqtt://{MQTT_BROKER_IP}:1883`
-const ACCESS_TOKEN = 'YC6o3ZWlSKhCBrI3TwRk' // device access token
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN // device access token
 
 const client = mqtt.connect(MQTT_URL, {
     username: ACCESS_TOKEN,
 })
 
-let temperature = 20
+let temp = 20
 
 client.on('connect', () => {
     console.log('connected to thingsboard mqtt')
     setInterval(() => {
-        temperature += 1 // 온도를 1씩 증가
-        const payload = JSON.stringify({ temperature })
-
+        temp += 1
+        const payload = JSON.stringify({ temp })
         client.publish('v1/devices/me/telemetry', payload, () => {
             console.log(`📡 Published: ${payload}`)
         })
-    }, 1000) // 1초마다 실행
+    }, 1000)
 })
 
 client.on('error', (err) => {
